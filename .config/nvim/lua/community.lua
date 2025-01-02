fff9bc88c4a12371ebe0b6754dee0e3a334888c9
Francisco Martinez <francisco@zenitram.dev>
2025-01-02 10:56:37 -0800

    Add mini-move

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -12,6 +12,7 @@ return {
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
   { import = "astrocommunity.code-runner.executor-nvim" },
+  { import = "astrocommunity.motion.mini-move" },
   -- { import = "astrocommunity.programming-language-support.rest-nvim" },
   -- { import = "astrocommunity.note-taking.obsidian-nvim" },
 }
